8f01f7e5f815f0e69ae5dee48ffe4bda9863ed77
Beck <chimyon72@gmail.com>
2025-10-19 10:06:51 +0900

    Initial commit: Django project setup

---
+++ stove_tracker/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for stove_tracker project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stove_tracker.settings')
+
+application = get_asgi_application()
